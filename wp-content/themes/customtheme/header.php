15c4beeae29f953fd5de5f20b3c28702e23d9aab
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-07-19 15:55:40 +0100

    basic styling applied

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo get_bloginfo( 'name' ); ?></title>
 
-    <link href="<?php echo get_bloginfo('template_directory');?>/style.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory');?>/assets/css/style.css" rel="stylesheet">
     
     <?php wp_head();?>
 </head>
@@ -15,7 +15,7 @@
             <a href="<?php echo get_bloginfo( 'wpurl' );?>">
                 <?php echo get_bloginfo( 'name' ); ?>
             </a>
-        <h1>
+        </h1>
         <p><?php bloginfo('description'); ?></p>
     </header>  
     <nav>
