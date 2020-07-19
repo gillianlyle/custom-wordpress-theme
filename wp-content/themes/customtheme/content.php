936ddaa20a027534c6fa7b211412a556f085d3da
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-07-19 12:29:26 +0100

    Display blog posts on home page.

---
+++ wp-content/themes/customtheme/content.php
@@ -0,0 +1,5 @@
+<article>
+    <h2><?php the_title(); ?></h2>
+	<p><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+    <?php the_content(); ?>
+</article>
--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-    Main Content
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); get_template_part( 'content', get_post_format() ); endwhile; endif; ?>
 </main>
 
 <?php get_footer(); ?>
