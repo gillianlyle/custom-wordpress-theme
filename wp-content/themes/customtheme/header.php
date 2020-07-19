32a2b01aca51758ce0a1855a16c6b0daee988005
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-07-19 11:53:24 +0100

    Title & description point to WP admin options

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Custom wordpress theme</title>
+    <title><?php echo get_bloginfo( 'name' ); ?></title>
 
     <link href="<?php echo get_bloginfo('template_directory');?>/style.css" rel="stylesheet">
     
@@ -11,6 +11,10 @@
 </head>
 <body>
     <header>
-        Header
-    </header>
-    
+        <h1>
+            <a href="<?php echo get_bloginfo( 'wpurl' );?>">
+                <?php echo get_bloginfo( 'name' ); ?>
+            </a>
+        <h1>
+        <p><?php bloginfo('description'); ?></p>
+    </header>  
