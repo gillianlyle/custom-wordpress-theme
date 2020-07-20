f009bb92104b28b046a83591a590c9aa84647eab
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-07-20 11:28:43 +0100

    move stylesheet to root, add post excerpt

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo get_bloginfo( 'name' ); ?></title>
 
-    <link href="<?php echo get_bloginfo('template_directory');?>/assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory');?>/style.css" rel="stylesheet">
     
     <?php wp_head();?>
 </head>
