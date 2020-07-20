df317c9c9969b08604ff1532c729df419925b755
Gillian Lyle <gillian.lyle@mcdermottgroup.co.uk>
2020-07-20 10:28:30 +0100

    Show post excerpt

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -1,7 +1,17 @@
 <?php get_header(); ?>
 
 <main class="posts">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); get_template_part( 'content', get_post_format() ); endwhile; endif; ?>
+    <?php
+        if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" class="post">
+            <h2><?php the_title(); ?></h2>
+            <?php the_excerpt(); ?>
+        </article>
+
+        <?php endwhile;
+        endif;
+    ?>
 </main>
 
 <?php get_footer(); ?>
